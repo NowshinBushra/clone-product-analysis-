10cfff36c0f69e7eb3a559efe71e91468995e6a4
NowshinBushra <nowshin.bushra399@gmail.com>
2022-04-06 00:10:32 +0600

    readme file

---
+++ src/components/Cards/Card.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+const Card = () => {
+    return (
+        <div>
+            <div className="card-group">
+  <div className="card">
+    <div className="card-body">
+      <h5 className="card-title">Card title</h5>
+      <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+    </div>
+  </div>
+  <div className="card">
+    <div className="card-body">
+      <h5 className="card-title">Card title</h5>
+      <p className="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+      <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+    </div>
+  </div>
+  <div className="card">
+    <div className="card-body">
+      <h5 className="card-title">Card title</h5>
+      <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+      <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+    </div>
+  </div>
+</div>
+        </div>
+    );
+};
+
+export default Card;
